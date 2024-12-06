6626e42c004feff6c842f4fc9c33ad153ac43bea
Koh Quan Wei Ivan <ivankohquanwei@gmail.com>
2024-12-06 15:22:49 +0800

    bug fixes

--- data/preprocessing.py
+++ data/preprocessing.py
@@ -3,7 +3,9 @@ from collections import Counter
 from sklearn.model_selection import train_test_split
 
 import nltk
-nltk.download('punkt')  # Ensure the Punkt tokenizer is downloaded
+
+nltk.download("punkt")  # Ensure the Punkt tokenizer is downloaded
+
 
 def tokenize(text):
     """
@@ -17,7 +19,8 @@ def tokenize(text):
     tokens = nltk.tokenize.word_tokenize(text)
     return tokens
 
-def build_vocabulary(caption_df, vocab_size=5000):
+
+def build_vocabulary(caption_df, vocab_size=5000, tokenizing_fn=tokenize):
     """
     Builds word-to-index and index-to-word mappings based on caption data.
     Args:
@@ -37,7 +40,7 @@ def build_vocabulary(caption_df, vocab_size=5000):
     ]
 
     # Tokenize all captions and count word frequencies
-    all_words = [token for caption in all_captions for token in tokenize(caption)]
+    all_words = [token for caption in all_captions for token in tokenizing_fn(caption)]
     word_counts = Counter(all_words)
 
     # Define special tokens
@@ -55,7 +58,10 @@ def build_vocabulary(caption_df, vocab_size=5000):
 
     return word2idx, idx2word, image_captions
 
-def convert_captions_to_sequences(image_captions, word2idx):
+
+def convert_captions_to_sequences(
+    image_captions, word2idx, special_token_mapping=None, tokenizing_fn=tokenize
+):
     """
     Converts captions to sequences of word indices.
     Args:
@@ -68,19 +74,35 @@ def convert_captions_to_sequences(image_captions, word2idx):
     captions_seqs = {}
     max_length = 0
 
+    if not special_token_mapping:
+        special_token_mapping = {
+            "start": "<start>",
+            "end": "<end>",
+            "pad": "<pad>",
+            "unk": "<unk>",
+        }
+
     for img_name, captions in image_captions.items():
         seqs = []
         for caption in captions:
             # Tokenize and add start and end tokens
-            tokens = ["<start>"] + tokenize(caption) + ["<end>"]
+            tokens = (
+                [special_token_mapping["start"]]
+                + tokenizing_fn(caption)
+                + [special_token_mapping["end"]]
+            )
             # Convert tokens to indices, use <unk> for unknown words
-            seq = [word2idx.get(token, word2idx["<unk>"]) for token in tokens]
+            seq = [
+                word2idx.get(token, word2idx[special_token_mapping["unk"]])
+                for token in tokens
+            ]
             seqs.append(seq)
             # Update maximum caption length
             max_length = max(max_length, len(seq))
         captions_seqs[img_name] = seqs
 
     return captions_seqs, max_length
+
 
 def get_splits(image_names, test_size=0.2):
     """
@@ -103,7 +125,10 @@ def get_splits(image_names, test_size=0.2):
     )
     return train_images, val_images, test_images
 
-def prepare_image2captions(image_ids, captions_seqs, idx2word):
+
+def prepare_image2captions(
+    image_ids, captions_seqs, idx2word, special_token_mapping=None
+):
     """
     Prepares a mapping from image IDs to their corresponding captions in word form.
     Args:
@@ -113,19 +138,27 @@ def prepare_image2captions(image_ids, captions_seqs, idx2word):
     Returns:
         image2captions (dict): Mapping from image filenames to their captions as word lists.
     """
+    if not special_token_mapping:
+        special_token_mapping = {
+            "start": "<start>",
+            "end": "<end>",
+            "pad": "<pad>",
+            "unk": "<unk>",
+        }
+    start_end_pad = (
+        special_token_mapping["start"],
+        special_token_mapping["end"],
+        special_token_mapping["pad"],
+    )
     image2captions = {}
     for img_id in image_ids:
         seqs = captions_seqs[img_id]
         captions_list = []
         for seq in seqs:
             # Convert indices back to words
-            caption = [idx2word.get(idx, "<unk>") for idx in seq]
+            caption = [idx2word.get(idx, special_token_mapping["unk"]) for idx in seq]
             # Remove special tokens
-            caption = [
-                word.lower()
-                for word in caption
-                if word not in ["<start>", "<end>", "<pad>"]
-            ]
+            caption = [word.lower() for word in caption if word not in start_end_pad]
             captions_list.append(caption)
         image2captions[img_id] = captions_list
     return image2captions
